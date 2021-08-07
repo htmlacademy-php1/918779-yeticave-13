4c0666227321eb1b1ca8dd956e38da6b5a0ecb85
Anton Romanov <609990@gmail.com>
2021-08-07 12:53:58 +0300

    Правки шаблонов

--- data.php
+++ data.php
@@ -45,4 +45,19 @@ $advertises = [
     ]
 ];
 
+function decorate_price ($input) {
+    $output = "";
+    $input = ceil($input);
+
+    if ($input >= 1000) {
+
+        $input = number_format($input, 0, '',' ');
+
+    }
+
+    $output = $input . " " . "<span>&#8381;</span>";
+
+    return $output;
+}
+
 ?>
