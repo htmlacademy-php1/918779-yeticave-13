2aba4dc3a716696a13cc0f306050c7d852fb025f
Anton Romanov <609990@gmail.com>
2021-08-13 19:19:14 +0300

    Правит схему БД

--- schema.sql
+++ schema.sql
@@ -4,34 +4,44 @@ CREATE DATABASE yeticave
 
 USE yeticave;
 
-CREATE TABLE category (
+CREATE TABLE categories (
 id INT AUTO_INCREMENT PRIMARY KEY,
 category_name VARCHAR(128) NOT NULL UNIQUE,
-code VARCHAR(128) NOT NULL UNIQUE
+category_code VARCHAR(128) NOT NULL UNIQUE
 );
 
-CREATE TABLE lot (
+CREATE TABLE lots (
 id INT AUTO_INCREMENT PRIMARY KEY,
-create_data TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+create_datе DATETIME DEFAULT CURRENT_TIMESTAMP,
 lot_name TEXT NOT NULL,
-lot_description TEXT NOT NULL,
+user_description TEXT NOT NULL,
 photo TEXT,
-start_price INT NOT NULL,
-date_expiration TIMESTAMP,
-lot_step INT NOT NULL
+price INT NOT NULL,
+date_expiration DATETIME,
+step INT NOT NULL,
+user_id INT,
+winner_id INT,
+category_id INT,
+FOREIGN KEY (user_id) REFERENCES users(id),
+FOREIGN KEY (winner_id) REFERENCES users(id),
+FOREIGN KEY (category_id) REFERENCES categories(id)
 );
 
-CREATE TABLE bet (
+CREATE TABLE bets (
 id INT AUTO_INCREMENT PRIMARY KEY,
-date_bet TIMESTAMP,
-sum_bet INT NOT NULL
+date_bet DATETIME,
+sum INT NOT NULL,
+user_id INT,
+lot_id INT,
+FOREIGN KEY (user_id) REFERENCES users(id),
+FOREIGN KEY (lot_id) REFERENCES lots(id)
 );
 
 CREATE TABLE users (
 id INT AUTO_INCREMENT PRIMARY KEY,
-user_born TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-email VARCHAR(128) NOT NULL UNIQUE,
-user_name VARCHAR(128) NOT NULL UNIQUE,
-user_password VARCHAR(128) NOT NULL UNIQUE,
-contacts VARCHAR(128)
+user_registration DATETIME DEFAULT CURRENT_TIMESTAMP,
+email VARCHAR(256) NOT NULL UNIQUE,
+user_name VARCHAR(128) NOT NULL,
+user_password VARCHAR(128) NOT NULL,
+contacts TEXT
 );
