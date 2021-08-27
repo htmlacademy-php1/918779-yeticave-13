d350ec36b635ea12c8595f4e0f52f4bc763f42d5
Anton Romanov <609990@gmail.com>
2021-08-27 13:22:41 +0300

    Добавляет правки

--- index.php
+++ index.php
@@ -2,36 +2,18 @@
 require_once('helpers.php');
 require_once('functions.php');
 require_once('init.php');
+require_once('data.php');
 
 if(!$link) {
+
     $error = mysqli_connect_error();
     $connect = include_template('error.php', ['error' => $error]);
+    http_response_code();
+    
 }   
 
 else {
-    // Запрос на получение списка категорий
-    $sql = 'SELECT category_code, category_name FROM categories';
-    
-    //Выполняем запрос и получаем результат
-    $result = mysqli_query($link, $sql);
-    
-    // Запрос выполнен успешно
-    if ($result) {
-    // Получаем все категории в виде двухмерного массива
-    
-    $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    
-    } else {
-    
-    // Получить текст последней ошибки
-    
-    $error = mysqli_error($link);
-    $content = include_template('error.php', ['error' => $error]);
-    
-    }
 
-}
-    
     //Запрос на показ лотов
     $sql = 'SELECT lots.lot_name, lots.price, lots.photo, lots.date_expiration, categories.category_name, MAX(bets.sum) as current_price 
     FROM lots JOIN categories ON lots.category_id=categories.id 
@@ -40,24 +22,31 @@ else {
     GROUP BY lots.id';
     
     $res = mysqli_query($link, $sql);
+
     if ($res) {
+
        $advertise = mysqli_fetch_all($res, MYSQLI_ASSOC);
+
     } else {
+        
        $error = mysqli_error($link);
+       http_response_code();
     }
 
-$main_content = include_template('main.php', [
-    'categories' => $categories,
-    'advertise' => $advertise
-]);
+    $main_content = include_template('main.php', [
+        'categories' => $categories,
+        'advertise' => $advertise
+    ]);
 
-$layout_content = include_template('layout.php', [
-    'is_auth' => $is_auth,
-    'content' => $main_content,
-    'categories' => $categories,
-    'title' => 'Главная',
-    'user_name' => $user_name
-]);
+    $layout_content = include_template('layout.php', [
+        'is_auth' => $is_auth,
+        'content' => $main_content,
+        'categories' => $categories,
+        'title' => 'Главная',
+        'user_name' => $user_name
+    ]);
+
+}
 
 print($layout_content);
 
--- init.php
+++ init.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once 'helpers.php';
-require_once 'functions.php';
-$db = require_once 'db.php';
+    require_once 'helpers.php';
+    require_once 'functions.php';
+    $db = require_once 'db.php';
 
-$link = mysqli_connect('localhost', 'root', '', 'yeticave');
-mysqli_set_charset($link, 'utf-8');
+    $link = mysqli_connect($db['localhost'], $db['user'], $db['purchase'], $db['data']);
+    mysqli_set_charset($link, 'utf-8');
 
 ?>
